8726d16de6c73f6ca3a62014ebc08a1fe7ab7e04
Raghav-Maheshwari2004 <maheshwari.raghav2004@gmail.com>
2025-02-22 00:33:11 +0530

    Update assests.js

--- src/assets/assests.js
+++ src/assets/assests.js
@@ -28,7 +28,7 @@ import screenshot4 from "./4.png"
 import screenshot5 from "./5.png"
 import screenshot6 from "./6.png"
 import screenshot7 from "./7.png"
-import photo from "./photo.jpg"
+import photo from "./photo.png"
 
 const assets = {
   cf1,
