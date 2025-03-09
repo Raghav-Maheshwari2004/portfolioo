c051cefb75bd2c99553c6ac29502eb896c4620df
Raghav-Maheshwari2004 <maheshwari.raghav2004@gmail.com>
2025-03-10 02:30:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Intro from "./Components/Intro/Intro";
 import About from "./Components/About/About";
 import Footer from "./Components/Footer/Footer";
-import { BrowserRouter } from "react-router-dom";
+import './styles/theme.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Navbar/>
-    <Intro/>
-    <About/>
-    <Footer/>
-    </BrowserRouter>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<>
+          <Intro />
+          <About />
+          <Footer />
+        </>} />
+        
+      </Routes>
+    </Router>
   );
 };
 
